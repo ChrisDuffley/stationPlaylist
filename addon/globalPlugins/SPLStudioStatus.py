d7881b59a1bfacfe7eb8869a211b8ee08c04681b
Joseph Lee <joseph.lee22590@gmail.com>
2013-12-05 04:33:54 -0800

    You can now press NVDA with grave key to switch to SPL Studio window from any application.

--- addon/globalPlugins/SPLStudioStatus.py
+++
@@ -1,17 +0,0 @@
-# Module for use with Stationplaylist Studio
-# Geoff Shang - April 2011
-
-# The primary function of this appModule is to provide meaningful feedback to users of SplStudio
-# by allowing speaking of items which cannot be easily found.
-
-# Version 0.01 - 7 April 2011:
-# Initial release: Jamie's focus hack plus auto-announcement of status items.
-
-import controlTypes
-import globalPluginHandler
-import api
-import ui
-
-class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-
-	pass
