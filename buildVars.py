f46054b9be0e279334cfb287ea21dd2990a973d1
Joseph Lee <joseph.lee22590@gmail.com>
2013-12-30 23:41:48 -0800

    beta 2: adds support for SPL 4.x. Some of the UI elements are different between 4.x and 5.x. Thus added logic to support both at once. The full support for SPL4 is scheduled for 1.1 in 2014.

--- buildVars.py
+++ buildVars.py
@@ -18,7 +18,7 @@ addon_info = {
 	"addon-description" : _("""Enhances support for Station Playlist Studio.
 In addition, adds global commands for the studio from everywhere."""),
 	# version
-	"addon-version" : "1.0-dev-20131222",
+	"addon-version" : "1.0-dev-20131231",
 	# Author(s)
 	"addon-author" : "Geoff Shang, Joseph Lee and other contributors",
 	# URL for the add-on documentation support
