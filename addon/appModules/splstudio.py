18ab57d227704c46d0ab95b5aead81e81e26d912
Joseph Lee <joseph.lee22590@gmail.com>
2014-01-24 23:56:33 -0800

    SPL app module: use screen text as names for certain edit fields. In certain edit fields, the actual label for the field is written to the screen, and there is no way to fetch the object for the label. To get around this, use review.getScreenPosition(), expand the text info and assign the textInfo.text as obj.name. So far works with SPL4. In SPL5, the label and the value for the edit field is located on the same line, so devise a way to announce only the label text. This fixes a long standing bug where no label for edit field was announced, particularly in Options dialogs.

--- addon/appModules/splstudio.py
+++ addon/appModules/splstudio.py
@@ -21,6 +21,7 @@ import wx
 import winUser
 from ctypes import windll
 from NVDAObjects.IAccessible import IAccessible
+import textInfos
 import tones
 from functools import wraps
 import addonHandler
@@ -65,6 +66,12 @@ class AppModule(appModuleHandler.AppModule):
 			obj.shouldAllowIAccessibleFocusEvent = False
 		# Radio button group names are not recognized as grouping, so work around this.
 		if obj.windowClassName == "TRadioGroup": obj.role = ROLE_GROUPING
+		# In certain edit fields, the field name is written to the screen, and there's no way to fetch the object for this text. Thus use review position text.
+		elif obj.windowClassName == "TEdit" and obj.name is None:
+			import review # This means at least NVDA 2013.2 is required.
+			fieldName, fieldObj  = review.getScreenPosition(obj)
+			fieldName.expand(textInfos.UNIT_LINE)
+			obj.name = fieldName.text
 
 	# Check the following variable for end of track announcement.
 	SPLEndOfTrackTime = "00:05" # Should be adjustable by the user in the end. Also find a way to announce this even if SPL Studio is minimized.
