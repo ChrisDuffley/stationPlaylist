d929d7a4c7e93a64b577520fe6cc38c841fbd67e
Noelia Ruiz Martínez <nrm1977@gmail.com>
2014-09-06 08:56:37 +0200

    Removed docHandler and add last addonTemplate.

--- buildVars.py
+++ buildVars.py
@@ -1,39 +1,43 @@
 # -*- coding: UTF-8 -*-
 
 # Build customizations
-# Change this file instead of sconstruct, whenever possible.
+# Change this file instead of sconstruct or manifest files, whenever possible.
 
 # Full getext (please don't change)
 _ = lambda x : x
 
 # Add-on information variables
 addon_info = {
-# add-on Name
-	"addon-name" : "stationPlaylist",
+	# for previously unpublished addons, please follow the community guidelines at:
+	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
+	# add-on Name, internal for nvda
+	"addon_name" : "stationPlaylist",
+	# Add-on summary, usually the user visible name of the addon.
+	# Translators: Summary for this add-on to be shown on installation and add-on information.
+	"addon_summary" : _("Station Playlist Studio"),
 	# Add-on description
-	# TRANSLATORS: Summary for this add-on to be shown on installation and add-on information.
-	"addon-summary" : _("Station Playlist Studio"),
-	# Add-on description
-	# Translators: Long description to be shown for this add-on on installation and add-on information
-	"addon-description" : _("""Enhances support for Station Playlist Studio.
+	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+	"addon_description" : _("""Enhances support for Station Playlist Studio.
 In addition, adds global commands for the studio from everywhere."""),
 	# version
-	"addon-version" : "3.0-dev",
+	"addon_version" : "3.0",
 	# Author(s)
-	"addon-author" : "Geoff Shang, Joseph Lee and other contributors",
+	"addon_author" : u"Geoff Shang, Joseph Lee and other contributors",
 	# URL for the add-on documentation support
-	"addon-url" : "http://addons.nvda-project.org/"
+	"addon_url" : "http://addons.nvda-project.org/",
+	# Documentation file name
+	"addon_docFileName" : "readme.html",
 }
+
 
 import os.path
 
 # Define the python files that are the sources of your add-on.
 # You can use glob expressions here, they will be expanded.
-pythonSources = [os.path.join("addon", "appModules", "*.py"),
-    os.path.join("addon", "globalPlugins", "*.py")]
+pythonSources = [os.path.join("addon", "appModules", "*.py"), os.path.join("addon", "globalPlugins", "*.py")]
 
 # Files that contain strings for translation. Usually your python sources
-i18nSources = pythonSources + ["buildVars.py", "docHandler.py"]
+i18nSources = pythonSources + ["buildVars.py"]
 
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
