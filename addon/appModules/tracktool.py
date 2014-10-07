0e91d2d0032b2218ef8f06bcdd6e3cb32433a96e
Joseph Lee <joseph.lee22590@gmail.com>
2014-10-07 08:04:41 -0700

    Track Tool (4.0-dev/legacy): improve column header lookup to prevent NVDA from reading extraneous information, also added a script to announce artist name. The artist name field is focus.name, so take care of this. If there is no name or description, announce that no tracks were added.

--- addon/appModules/tracktool.py
+++ addon/appModules/tracktool.py
@@ -31,64 +31,75 @@ class AppModule(appModuleHandler.AppModule):
 	# Columns headers list:
 	# Number: start substring, ending substring.
 	columnHeaders = {
-		1: ["Title:", ", Duration:"], #Title
-		2: ["Duration:", ", Cue:"], #Duration
-		3: ["Cue:", ", Overlap:"], #Cue
-		4: ["Overlap:", ", Intro:"], #Overlap
-		5: ["Intro:", ", Segue:"], #Intro
-		6: ["Segue:", ", Filename:"], # Segue
-		7: ["Filename:", None] # Actual file name.
+		2: ["Title:", ", Duration:"], #Title
+		3: ["Duration:", ", "], #Duration
+		4: ["Cue:", ", "], #Cue
+		5: ["Overlap:", ", "], #Overlap
+		6: ["Intro:", ", "], #Intro
+		7: ["Segue:", ", "], # Segue
+		8: ["Filename:", None] # Actual file name.
 	}
 
 	def announceColumnHeader(self, column):
 		focus = api.getFocusObject()
 		if focus.windowClassName != "TListView" and focus.role != ROLE_LISTITEM:
 			ui.message("Not in tracks list")
+		elif focus.name is None and focus.description is None:
+			ui.message("No tracks added")
 		else:
-			desc = focus.description
-			colstr = self.columnHeaders[column][0]
-			colstrindex = desc.find(colstr)
-			if colstrindex < 0:
-				if colstr == "Intro:":
-					ui.message("Introduction not set")
+			if column != 1:
+				desc = focus.description
+				colstr = self.columnHeaders[column][0]
+				if colstr not in desc:
+					if colstr == "Intro:":
+						columnInfo = "Introduction not set"
+					else:
+						columnInfo = "{columnInfo} not found".format(columnInfo = colstr[:-1])
 				else:
-					ui.message("{columnInfo} not found".format(columnInfo = colstr[:-1]))
+					colstrindex = desc.find(colstr)
+					if column == 8:
+						columnInfo = desc[colstrindex:]
+					else:
+						colstrend = colstrindex+desc[colstrindex:].find(self.columnHeaders[column][1])
+						columnInfo = desc[colstrindex:colstrend]
 			else:
-				if column != 7:
-					colstrend = desc.find(self.columnHeaders[column][1])
-				if column != 7:
-					columnInfo = desc[colstrindex:colstrend]
+				if focus.name is None:
+					columnInfo = "No artist"
 				else:
-					columnInfo = columnInfo = desc[colstrindex:]
-				ui.message(columnInfo)
+					columnInfo = "Artist: {artistName}".format(artistName = focus.name)
+			ui.message(columnInfo)
 
-	def script_announceTitle(self, gesture):
+	def script_announceArtist(self, gesture):
 		self.announceColumnHeader(1)
 
-	def script_announceDuration(self, gesture):
+	def script_announceTitle(self, gesture):
 		self.announceColumnHeader(2)
 
-	def script_announceCue(self, gesture):
+	def script_announceDuration(self, gesture):
 		self.announceColumnHeader(3)
 
-	def script_announceOverlap(self, gesture):
+	def script_announceCue(self, gesture):
 		self.announceColumnHeader(4)
 
-	def script_announceIntro(self, gesture):
+	def script_announceOverlap(self, gesture):
 		self.announceColumnHeader(5)
 
-	def script_announceSegue(self, gesture):
+	def script_announceIntro(self, gesture):
 		self.announceColumnHeader(6)
 
-	def script_announceFilename(self, gesture):
+	def script_announceSegue(self, gesture):
 		self.announceColumnHeader(7)
 
+	def script_announceFilename(self, gesture):
+		self.announceColumnHeader(8)
+
 	__gestures={
-		"kb:control+NVDA+1":"announceTitle",
-		"kb:control+NVDA+2":"announceDuration",
-		"kb:control+NVDA+3":"announceCue",
-		"kb:control+NVDA+4":"announceOverlap",
-		"kb:control+NVDA+5":"announceIntro",
-		"kb:control+NVDA+6":"announceSegue",
-		"kb:control+NVDA+7":"announceFilename",
+		"kb:control+NVDA+1":"announceArtist",
+		"kb:control+NVDA+2":"announceTitle",
+		"kb:control+NVDA+3":"announceDuration",
+		"kb:control+NVDA+4":"announceCue",
+		"kb:control+NVDA+5":"announceOverlap",
+		"kb:control+NVDA+6":"announceIntro",
+		"kb:control+NVDA+7":"announceSegue",
+		"kb:control+NVDA+8":"announceFilename",
 	}
