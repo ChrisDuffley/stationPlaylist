b80e8d1033ffafa84a5f00bfc8bcf88441640bc2
Joseph Lee <joseph.lee22590@gmail.com>
2014-12-02 16:40:47 -0800

    Braille Timer (4.0/legacy): allow braille timer to show remaining time for the track and its intro regardless of alarm time value.

--- addon/appModules/splstudio.py
+++ addon/appModules/splstudio.py
@@ -198,13 +198,13 @@ class AppModule(appModuleHandler.AppModule):
 						tones.beep(512, 400)
 				elif obj.simplePrevious != None and obj.simplePrevious.name == "Remaining Time":
 					# End of track for SPL 5.x.
-					if self.brailleTimer in [self.brailleTimerEnding, self.brailleTimerBoth] and "00:00" < obj.name <= self.SPLEndOfTrackTime:
+					if self.brailleTimer in [self.brailleTimerEnding, self.brailleTimerBoth]: #and "00:00" < obj.name <= self.SPLEndOfTrackTime:
 						braille.handler.message(obj.name)
 					if obj.name == self.SPLEndOfTrackTime:
 						tones.beep(440, 200)
 				elif obj.simplePrevious != None and obj.simplePrevious.name == "Remaining Song Ramp":
 					# Song intro for SPL 5.x.
-					if self.brailleTimer in [self.brailleTimerIntro, self.brailleTimerBoth] and "00:00" < obj.name <= self.SPLSongRampTime:
+					if self.brailleTimer in [self.brailleTimerIntro, self.brailleTimerBoth]: #and "00:00" < obj.name <= self.SPLSongRampTime:
 						braille.handler.message(obj.name)
 					if obj.name == self.SPLSongRampTime:
 						tones.beep(512, 400)
