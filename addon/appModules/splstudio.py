aa61db2a940b486879c2bc673de0d358d00af361
Noelia Ruiz Martínez <nrm1977@gmail.com>
2014-12-12 18:32:30 +0100

    Possible improvements cleaning code.

--- addon/appModules/splstudio.py
+++ addon/appModules/splstudio.py
@@ -126,68 +126,67 @@ class AppModule(appModuleHandler.AppModule):
 		# Do not let NvDA get name for None object when SPL window is maximized.
 		if not obj.name:
 			return
-		else:
-			if obj.windowClassName == "TStatusBar" and not obj.name.startswith("  Up time:"):
-				# Special handling for Play Status
-				fgWinClass = api.getForegroundObject().windowClassName
-				if obj.IAccessibleChildID == 1:
-					if fgWinClass == "TStudioForm":
-						# Strip off "  Play status: " for brevity only in main playlist window.
-						ui.message(obj.name[15:])
-					elif fgWinClass == "TTrackInsertForm" and self.libraryScanProgress > 0:
-						# If library scan is in progress, announce its progress.
-						self.scanCount+=1
-						if self.scanCount%100 == 0:
-							if self.libraryScanProgress == self.libraryScanMessage:
-								tones.beep(550, 100) if self.beepAnnounce else ui.message("Scanning")
-							elif self.libraryScanProgress == self.libraryScanNumbers:
-								if self.beepAnnounce: tones.beep(550, 100)
-								ui.message(obj.name[1:obj.name.find("]")])
-						if "Loading" in obj.name and not self.libraryScanning:
-							self.libraryScanning = True
-						elif "match" in obj.name and self.libraryScanning:
-							tones.beep(370, 100) if self.beepAnnounce else ui.message("Scan complete")
-							self.libraryScanning = False
-							self.scanCount = 0
+		if obj.windowClassName == "TStatusBar" and not obj.name.startswith("  Up time:"):
+			# Special handling for Play Status
+			fgWinClass = api.getForegroundObject().windowClassName
+			if obj.IAccessibleChildID == 1:
+				if fgWinClass == "TStudioForm":
+					# Strip off "  Play status: " for brevity only in main playlist window.
+					ui.message(obj.name[15:])
+				elif fgWinClass == "TTrackInsertForm" and self.libraryScanProgress > 0:
+					# If library scan is in progress, announce its progress.
+					self.scanCount+=1
+					if self.scanCount%100 == 0:
+						if self.libraryScanProgress == self.libraryScanMessage:
+							tones.beep(550, 100) if self.beepAnnounce else ui.message("Scanning")
+						elif self.libraryScanProgress == self.libraryScanNumbers:
+							if self.beepAnnounce: tones.beep(550, 100)
+							ui.message(obj.name[1:obj.name.find("]")])
+					if "Loading" in obj.name and not self.libraryScanning:
+						self.libraryScanning = True
+					elif "match" in obj.name and self.libraryScanning:
+						tones.beep(370, 100) if self.beepAnnounce else ui.message("Scan complete")
+						self.libraryScanning = False
+						self.scanCount = 0
 				else:
+					# Even with beeps enabled, be sure to announce scheduled time and name of the playing cart.
+					if obj.name.startswith("Scheduled for") or obj.name.startswith("Cart") and obj.IAccessibleChildID == 3:
+						ui.message(obj.name)
+					elif not (obj.name.endswith(" On") or obj.name.endswith(" Off")):
+						# Announce status information that does not contain toggle messages.
+						ui.message(obj.name)
 					if self.beepAnnounce:
-						# Even with beeps enabled, be sure to announce scheduled time and name of the playing cart.
-						if obj.name.startswith("Scheduled for") or obj.name.startswith("Cart") and obj.IAccessibleChildID == 3:
-							ui.message(obj.name)
-						elif not (obj.name.endswith(" On") or obj.name.endswith(" Off")):
-							# Announce status information that does not contain toggle messages.
-							ui.message(obj.name)
-						else:
-							# User wishes to hear beeps instead of words. The beeps are power on and off sounds from PAC Mate Omni.
-							beep = obj.name.split(" ")
-							stat = beep[-1]
-							wavDir = os.path.dirname(__file__)
-							# Play a wave file based on on/off status.
-							if stat == "Off":
-								wavFile = os.path.join(wavDir, "SPL_off.wav")
-							elif stat == "On":
-								wavFile = os.path.join(wavDir, "SPL_on.wav")
-							messageSound(wavFile, obj.name)
+						# User wishes to hear beeps instead of words. The beeps are power on and off sounds from PAC Mate Omni.
+						beep = obj.name.split(" ")
+						stat = beep[-1]
+						wavDir = os.path.dirname(__file__)
+						# Play a wave file based on on/off status.
+						if stat == "Off":
+							wavFile = os.path.join(wavDir, "SPL_off.wav")
+						elif stat == "On":
+							wavFile = os.path.join(wavDir, "SPL_on.wav")
+						messageSound(wavFile, obj.name)
 					else:
 						ui.message(obj.name)
-					if self.cartExplorer or self.micAlarm:
-						# Activate mic alarm or announce when cart explorer is active.
-						self.doExtraAction(obj.name)
-			# Monitor the end of track and song intro time and announce it.
-			elif obj.windowClassName == "TStaticText": # For future extensions.
-				if obj.simplePrevious != None and obj.simplePrevious.name == "Remaining Time":
+				if self.cartExplorer or self.micAlarm:
+					# Activate mic alarm or announce when cart explorer is active.
+					self.doExtraAction(obj.name)
+		# Monitor the end of track and song intro time and announce it.
+		elif obj.windowClassName == "TStaticText": # For future extensions.
+			if obj.simplePrevious != None:
+				if obj.simplePrevious.name == "Remaining Time":
 					# End of track for SPL 5.x.
 					if self.brailleTimer in [self.brailleTimerEnding, self.brailleTimerBoth]: #and "00:00" < obj.name <= self.SPLEndOfTrackTime:
 						braille.handler.message(obj.name)
 					if obj.name == self.SPLEndOfTrackTime:
 						tones.beep(440, 200)
-				elif obj.simplePrevious != None and obj.simplePrevious.name == "Remaining Song Ramp":
+				if obj.simplePrevious.name == "Remaining Song Ramp":
 					# Song intro for SPL 5.x.
 					if self.brailleTimer in [self.brailleTimerIntro, self.brailleTimerBoth]: #and "00:00" < obj.name <= self.SPLSongRampTime:
 						braille.handler.message(obj.name)
 					if obj.name == self.SPLSongRampTime:
 						tones.beep(512, 400)
-			# Clean this mess with a more elegant solution.
+		# Clean this mess with a more elegant solution.
 		nextHandler()
 
 	# JL's additions
@@ -195,7 +194,7 @@ class AppModule(appModuleHandler.AppModule):
 	# Perform extra action in specific situations (mic alarm, for example).
 	def doExtraAction(self, status):
 		global micAlarmT
-		if self.cartExplorer:	
+		if self.cartExplorer:
 			if status == "Cart Edit On":
 				# Translators: Presented when cart edit mode is toggled on while cart explorer is on.
 				ui.message(_("Cart explorer is active"))
@@ -229,7 +228,6 @@ class AppModule(appModuleHandler.AppModule):
 
 	# Save configuration when terminating.
 	def terminate(self):
-		global SPLConfig
 		if SPLConfig is not None: SPLConfig.write()
 
 	# Script sections (for ease of maintenance):
