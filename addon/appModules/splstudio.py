be947527d0509650776345042e1cbe36272ed1fb
Joseph Lee <joseph.lee22590@gmail.com>
2014-12-12 22:29:41 -0800

    4.0 beta 2: NVDA will no longer play error tones whne restarted while Studio 5.10 is being used. Again, never rely on the fact that foreground window returns something. In this case, ever since overlay class was introduced, error tones were heard when NVDA was restarted while STudio 5.10 was focused. Thus check that we are dealing with a real foreground object when instantiating the overlay class.

--- addon/appModules/splstudio.py
+++ addon/appModules/splstudio.py
@@ -113,9 +113,10 @@ class AppModule(appModuleHandler.AppModule):
 	# Some controls which needs special routines.
 	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
 		fg = api.getForegroundObject()
-		role = obj.role
-		if obj.windowClassName == "TTntListView.UnicodeClass" and fg.windowClassName == "TStudioForm" and role == controlTypes.ROLE_LISTITEM and obj.name is not None:
-			clsList.insert(0, SPL510TrackItem)
+		if fg:
+			role = obj.role
+			if obj.windowClassName == "TTntListView.UnicodeClass" and fg.windowClassName == "TStudioForm" and role == controlTypes.ROLE_LISTITEM and obj.name is not None:
+				clsList.insert(0, SPL510TrackItem)
 
 	# Check the following variable for end of track announcement.
 	SPLEndOfTrackTime = "00:05"
