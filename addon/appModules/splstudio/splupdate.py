d0515648e58de5ee8855c613baed18bb0be5131e
Joseph Lee <joseph.lee22590@gmail.com>
2015-12-07 17:35:17 -0800

    Auto update check (7.0-dev): If a socket error occurs, retry after a while.
    If an Internet connection is lost, urllib throws IOError (tecnically, a socket error). Ifthis happens, the add-on will wait ten minutes before trying again, and in the process, set a flag reminding itself to check for update after a failure. Once this flag is set, update check time will not be set until a successful connection is made.

--- addon/appModules/splstudio/splupdate.py
+++ addon/appModules/splstudio/splupdate.py
@@ -28,6 +28,8 @@ SPLUpdateURL = "http://addons.nvda-project.org/files/get.php?file=spl-dev"
 _SPLUpdateT = None
 # How long it should wait between automatic checks.
 _updateInterval = 86400
+# Set if a socket error occurs.
+_retryAfterFailure = False
 
 def _versionFromURL(url):
 	filename = url.split("/")[-1]
@@ -66,11 +68,12 @@ def updateQualify(url):
 # The update check routine.
 # Auto is whether to respond with UI (manual check only), continuous takes in auto update check variable for restarting the timer.
 def updateCheck(auto=False, continuous=False):
-	global _SPLUpdateT, SPLAddonCheck
+	global _SPLUpdateT, SPLAddonCheck, _retryAfterFailure
 	# Regardless of whether it is an auto check, update the check time.
-	SPLAddonCheck = time.time()
+	# However, this shouldnt' be done if this is a retry after a failed attempt.
+	if not _retryAfterFailure: SPLAddonCheck = time.time()
 	# Should the timer be set again?
-	if continuous: _SPLUpdateT.Start(_updateInterval*1000, True)
+	if continuous and not _retryAfterFailure: _SPLUpdateT.Start(_updateInterval*1000, True)
 	# Auto disables UI portion of this function if no updates are pending.
 	if not auto: tones.beep(110, 40)
 	# Try builds does not (and will not) support upgrade checking unless absolutely required.
@@ -79,19 +82,37 @@ def updateCheck(auto=False, continuous=False):
 		progressTone = wx.PyTimer(updateProgress)
 		progressTone.Start(1000)
 	updateCandidate = False
-	url = urllib.urlopen(SPLUpdateURL)
-	url.close()
+	try:
+		url = urllib.urlopen(SPLUpdateURL)
+		url.close()
+	except IOError:
+		_retryAfterFailure = True
+		if not auto:
+			progressTone.Stop()
+			wx.CallAfter(gui.messageBox, "Error checking for update.", "Check for add-on update", wx.ICON_ERROR)
+		if continuous: _SPLUpdateT.Start(600000, True)
+		return
+	if _retryAfterFailure:
+		_retryAfterFailure = False
+		# Now is the time to update the check time if this is a retry.
+		SPLAddonCheck = time.time()
 	if url.code != 200:
-		if auto: return # No need to interact with the user.
+		if auto:
+			if continuous: _SPLUpdateT.Start(_updateInterval*1000, True)
+			return # No need to interact with the user.
 		checkMessage = "Add-on update check failed."
 	else:
 		# Am I qualified to update?
 		qualified = updateQualify(url)
 		if qualified is None:
-			if auto: return
+			if auto:
+				if continuous: _SPLUpdateT.Start(_updateInterval*1000, True)
+				return
 			checkMessage = "No add-on update available."
 		elif qualified == "":
-			if auto: return
+			if auto:
+				if continuous: _SPLUpdateT.Start(_updateInterval*1000, True)
+				return
 			checkMessage = "You appear to be running a version newer than the latest released version. Please reinstall the official version to downgrade."
 		else:
 			checkMessage = "Studio add-on {newVersion} ({modifiedDate}) is available. Would you like to update?".format(newVersion = qualified, modifiedDate = _lastModified(url.info().getheader("Last-Modified")))
