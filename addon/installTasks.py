776bf9e573b2973d54712f9d0d614ca44d1e5426
Joseph Lee <joseph.lee22590@gmail.com>
2016-12-27 18:43:18 -0800

    Provide a transition routine to move gestures from SPLStudioUtils to splUtils.
    In order to provide a seamless transition to splUtils global plugin, a transition algorithm has been implement that retrieves current gesture and adds new gestures under globalPlugins.splUtils module entry in gestures.ini (user gestures). This means breaking backwards compatibility and downgrades from 17.04 onwards.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -5,7 +5,7 @@
 # Routines are partly based on other add-ons, particularly Place Markers by Noelia Martinez (thanks add-on authors).
 
 def onInstall():
-	import os, shutil
+	import os, shutil, inputCore
 	profiles = os.path.join(os.path.dirname(__file__), "..", "stationPlaylist", "profiles")
 	# Import old profiles.
 	if os.path.exists(profiles):
@@ -16,3 +16,16 @@ def onInstall():
 			pass
 	# 7.4 only: prepare LTS presentation file (an empty text file)
 	#open(os.path.join(os.path.dirname(__file__), "ltsprep"), "w").close()
+	# 17.04 only: Global Plugin module name has changed, so pull in the old gestures if defined.
+	userGestures = inputCore.manager.userGestureMap._map
+	# Is the new module in there?
+	if "globalPlugins.splUtils" not in [script[0][0] for script in userGestures.values()]:
+		for gesture, script in userGestures.iteritems():
+			if script[0][0] == "globalPlugins.SPLStudioUtils":
+				# Prevent repeated gesture assignment.
+				try:
+					inputCore.manager.userGestureMap.remove(gesture, "globalPlugins.splUtils", script[0][1], script[0][2])
+				except ValueError:
+					pass
+				inputCore.manager.userGestureMap.add(gesture, "globalPlugins.splUtils", script[0][1], script[0][2])
+		inputCore.manager.userGestureMap.save()
