24b3930c5b017640a7b1a9271851d126a0511107
Joseph Lee <joseph.lee22590@gmail.com>
2017-09-30 21:01:31 -0700

    Extension points/Action (17.12): introduce extensionPoints.Action. re #40.
    In NVDA 2017.4, one can specify extension points and be notified of things happening such as actions, profile switches and what not. In SPL add-on, several actions are defined in a new module named splactions, including app ready, profile loaded/saved, broadcast profile switch and others. To maximize compatibility, extension points will be used if running on NVDA 2017.4 or later.

---
+++ addon/appModules/splstudio/splactions.py
@@ -0,0 +1,32 @@
+# SPL Studio Actions
+# An app module and global plugin package for NVDA
+# Copyright 2017 Joseph Lee and others, released under GPL.
+# Defines various actions to be carried out by various add-on modules, powered by extension points.
+# This module provides services for other modules, not the other way around.
+# Thus, importing other add-on modules should not be attempted unless granted.
+
+# An action is a notification about something that has happened or about to happen.
+# In SPL add-on, an action is prefixed by SPLAction*.
+
+import sys
+py3 = sys.version.startswith("3")
+
+# Do not unlock the full power of action extension point until 2018.
+try:
+	import extensionPoints
+	actionsAvailable = True
+except ImportError:
+	actionsAvailable = False
+
+# 40 (17.12): unconditionally define actions, but not until 2018.
+if actionsAvailable:
+	# Studio handle found, app module is fully ready.
+	SPLActionAppReady = extensionPoints.Action()
+	# Add-on settings were loaded.
+	SPLActionSettingsLoaded = extensionPoints.Action()
+	# Switching broadcast profiles.
+	SPLActionProfileSwitched = extensionPoints.Action()
+	# Settings are being saved.
+	SPLActionSettingsSaved = extensionPoints.Action()
+	# Studio is terminating.
+	SPLActionAppTerminating = extensionPoints.Action()
