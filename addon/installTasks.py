0b217ffa1743f8edaaf290d5cb8370e903eba9f6
Joseph Lee <joseph.lee22590@gmail.com>
2017-12-02 11:59:45 -0800

    Installer: command migration from 17.03 and earlier is no longer possible.
    With the new module name for global plugin, commands were migrated. This is no longer possible as of 18.01.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -18,7 +18,7 @@ def onInstall():
 			# Translators: Title of a dialog shown when installing Studio add-on on old Windows releases.
 			_("Old Windows version"), wx.OK | wx.ICON_ERROR)
 		raise RuntimeError("SPL: minimum Windows version requirement not met, aborting")
-	import os, shutil, inputCore
+	import os, shutil
 	profiles = os.path.join(os.path.dirname(__file__), "..", "stationPlaylist", "profiles")
 	# Import old profiles.
 	if os.path.exists(profiles):
@@ -29,16 +29,3 @@ def onInstall():
 			pass
 	# 7.4 only: prepare LTS presentation file (an empty text file)
 	#open(os.path.join(os.path.dirname(__file__), "ltsprep"), "w").close()
-	# 17.04 only: Global Plugin module name has changed, so pull in the old gestures if defined.
-	userGestures = inputCore.manager.userGestureMap._map
-	# Is the new module in there?
-	if "globalPlugins.splUtils" not in [script[0][0] for script in list(userGestures.values())]:
-		for gesture, script in userGestures.items():
-			if script[0][0] == "globalPlugins.SPLStudioUtils":
-				# Prevent repeated gesture assignment.
-				try:
-					inputCore.manager.userGestureMap.remove(gesture, "globalPlugins.splUtils", script[0][1], script[0][2])
-				except ValueError:
-					pass
-				inputCore.manager.userGestureMap.add(gesture, "globalPlugins.splUtils", script[0][1], script[0][2])
-		inputCore.manager.userGestureMap.save()
