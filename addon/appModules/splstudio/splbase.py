ffe50e3ee77a237d513b40b9b4fce7a995d90240
Joseph Lee <joseph.lee22590@gmail.com>
2018-01-04 11:44:39 -0800

    Studio API wrapper (18.02): introduce a new splbase module. Re #45.
    The SPL base services (splbase) will be used to house common functions and constants (and vars) used through Studio app module, mostly having to do with Studio API.

---
+++ addon/appModules/splstudio/splbase.py
@@ -0,0 +1,35 @@
+# SPL Studio base services
+# An app module and global plugin package for NVDA
+# Copyright 2018 Joseph Lee and others, released under GPL.
+
+# Base services for Studio app module and support modules
+
+import ui
+from winUser import sendMessage
+from .spldebugging import debugOutput
+
+# Cache the handle to main Studio window.
+_SPLWin = None
+
+# Use SPL Studio API to obtain needed values.
+# A thin wrapper around user32.SendMessage function with Studio handle and WM_USER supplied.
+# #45 (18.02): returns whatever result SendMessage function says.
+# If debugging framework is on, print arg, command and other values.
+def studioAPI(arg, command):
+	if _SPLWin is None:
+		debugOutput("Studio handle not found")
+		return
+	debugOutput("Studio API wParem is %s, lParem is %s"%(arg, command))
+	val = sendMessage(_SPLWin, 1024, arg, command)
+	debugOutput("Studio API result is %s"%val)
+	return val
+
+# Check if Studio itself is running.
+# This is to make sure custom commands for SPL Assistant commands and other app module gestures display appropriate error messages.
+def studioIsRunning():
+	if _SPLWin is None:
+		debugOutput("Studio handle not found")
+		# Translators: A message informing users that Studio is not running so certain commands will not work.
+		ui.message(_("Studio main window not found"))
+		return False
+	return True
