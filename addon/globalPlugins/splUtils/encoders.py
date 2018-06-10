c030eb38e0579f5029af2202b66d8e9ddfe1b0a5
Joseph Lee <joseph.lee22590@gmail.com>
2018-06-10 16:02:58 -0700

    18.06.1/encoders: stream labeler dialog fixes from wxPython 4.
    wxPython's text entry dialog no longer users defaultValue to set initial string, the value keyword is used instead.

--- addon/globalPlugins/splUtils/encoders.py
+++ addon/globalPlugins/splUtils/encoders.py
@@ -362,8 +362,13 @@ class Encoder(IAccessible):
 		streamTitle = _("Stream labeler for {streamEntry}").format(streamEntry = title)
 		# Translators: The text of the stream labeler dialog.
 		streamText = _("Enter the label for this stream")
-		dlg = wx.TextEntryDialog(gui.mainFrame,
-		streamText, streamTitle, defaultValue=curStreamLabel)
+		# In wxPython 4's text entry dialog, defaultValue is replaced by value keyword.
+		if wx.version().startswith("4"):
+			dlg = wx.TextEntryDialog(gui.mainFrame,
+			streamText, streamTitle, value=curStreamLabel)
+		else:
+			dlg = wx.TextEntryDialog(gui.mainFrame,
+			streamText, streamTitle, defaultValue=curStreamLabel)
 		def callback(result):
 			if result == wx.ID_OK:
 				newStreamLabel = dlg.GetValue()
