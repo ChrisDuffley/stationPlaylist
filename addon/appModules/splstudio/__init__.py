7ad9fc86d21fcac453dc9dd8513eecc3f03c7ed7
Joseph Lee <joseph.lee22590@gmail.com>
2018-10-26 21:28:50 -0700

    Startup: detect true startup versus NVDA restart and record this fact. Fixes #84.
    Detect if this is a true startup (Studio has just started) or this is NVDA restating (Studio was already running). Then record this fact via a debug message so it can be used for various troubleshooting steps for startup and related problems.
    This will be backported to LTS18 in 2019.

--- addon/appModules/splstudio/__init__.py
+++ addon/appModules/splstudio/__init__.py
@@ -636,6 +636,11 @@ class AppModule(appModuleHandler.AppModule):
 		super(AppModule, self).__init__(*args, **kwargs)
 		if self.SPLCurVersion < SPLMinVersion:
 			raise RuntimeError("Unsupported version of Studio is running, exiting app module")
+		debugOutput("Using SPL Studio version %s"%self.SPLCurVersion)
+		# #84: if foreground object is defined, this is a true Studio start, otherwise this is an NVDA restart with Studio running.
+		# The latter is possible because app module constructor can run before NVDA finishes initializing, particularly if system focus is located somewhere other than Taskbar.
+		# Note that this is an internal implementation detail and is subject to change without notice.
+		debugOutput("Studio is starting" if api.getForegroundObject() is not None else "Studio is already running")
 		# 17.09: do this if minimal startup flag is not present.
 		try:
 			if not globalVars.appArgs.minimal:
