f7103d938f3966855ddb569309186774f5bfc41c
Joseph Lee <joseph.lee22590@gmail.com>
2018-11-30 21:11:23 -0800

    BuildVars: minimum and last tested versions set to 2018.4 and 2019.2

--- buildVars.py
+++ buildVars.py
@@ -28,9 +28,9 @@ In addition, adds global commands for the studio from everywhere."""),
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
 	# Minimum NVDA version supported
-	"addon_minimumNVDAVersion" : "2018.3",
+	"addon_minimumNVDAVersion" : "2018.4",
 	# Last NVDA version supported/tested
-	"addon_lastTestedNVDAVersion" : "2019.1",
+	"addon_lastTestedNVDAVersion" : "2019.2",
 	# Add-on update channel (default is stable)
 	"addon_updateChannel" : None,
 }
