f475570f9241b38b3bdcff27e541afb314e6d265
Joseph Lee <joseph.lee22590@gmail.com>
2019-02-16 21:02:58 -0800

    Config: move isDevVersion function to outside of ConfigHub.
    Because other modules might check if the dev version of the add-on is running (especially without SPLConfig loaded), move dev version checker to outside of the config hub class.

--- addon/appModules/splstudio/splconfig.py
+++ addon/appModules/splstudio/splconfig.py
@@ -117,6 +117,11 @@ defaultProfileName = _("Normal profile")
 # StationPlaylist components.
 _SPLComponents_ = ("splstudio", "splcreator", "tracktool")
 
+# There are times when a feature must be tested by more users without introducing regressions to stable branch users.
+def isDevVersion():
+	SPLAddonManifest = addonHandler.Addon(os.path.join(os.path.dirname(__file__), "..", "..")).manifest
+	return SPLAddonManifest['updateChannel'] == "dev"
+
 # 8.0: Run-time config storage and management will use ConfigHub data structure, a subclass of chain map.
 # A chain map allows a dictionary to look up predefined mappings to locate a key.
 # When mutating a value, chain map defaults to using the topmost (zeroth) map, which isn't desirable if one wishes to use a specific map.
@@ -671,15 +676,9 @@ class ConfigHub(ChainMap):
 
 	# 18.09: determine if pilot features can be used.
 	# 19.03: split into two functions: the test drive checker function and a dev channel checker.
-	# There are times when a feature must be tested by more users without introducing regressions to stable branch users.
-	@property
-	def isDevVersion(self):
-		SPLAddonManifest = addonHandler.Addon(os.path.join(os.path.dirname(__file__), "..", "..")).manifest
-		return SPLAddonManifest['updateChannel'] == "dev"
-
 	@property
 	def canEnablePilotFeatures(self):
-		return self.isDevVersion and not self._pendingPilotFeaturesToggle
+		return isDevVersion() and not self._pendingPilotFeaturesToggle
 
 	@property
 	def testDrive(self):
