b2a0d8259422ac345994e47bef1457a9a672eed2
Joseph Lee <joseph.lee22590@gmail.com>
2019-03-20 11:00:07 -0700

    SPL track items: catch possible COM error (and possibly others) while fetching item description.
    Reported by a user: while using SPL Creator, NVDA fails to announce names of some lists, traced to a COM error. Thus catch this and other exceptions while fetching item description.
    This will be backported to LTS18.

--- addon/appModules/splstudio/__init__.py
+++ addon/appModules/splstudio/__init__.py
@@ -101,7 +101,11 @@ class SPLTrackItem(sysListView32.ListItem):
 
 	def _get_description(self):
 		# SysListView32.ListItem nullifies description, so resort to fetching it via IAccessible.
-		return self.IAccessibleObject.accDescription(self.IAccessibleChildID)
+		# 19.04/18.09.8-LTS: sometimes, description must be None because it is already dealing with what appears to be a SysListView32 object, resulting in COM error.
+		try:
+			return self.IAccessibleObject.accDescription(self.IAccessibleChildID)
+		except:
+			return ""
 
 	def script_moveToNextColumn(self, gesture):
 		if (self._curColumnNumber+1) == self.parent.columnCount:
