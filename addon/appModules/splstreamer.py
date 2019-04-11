c03cb4f51480c0ccaa0f7bba7b579290fa24482d
Joseph Lee <joseph.lee22590@gmail.com>
2019-04-11 01:13:07 -0700

    Initial support for SPL Streamer.
    SPL Streamer is a standalone app for streaming broadcasts.
    This initial commit allows read-only fields in SPL Streamer main window to receive labels through an overlay class.

---
+++ addon/appModules/splstreamer.py
@@ -0,0 +1,23 @@
+# StationPlaylist Streamer
+# An app module and global plugin package for NVDA
+# Copyright 2019 Joseph Lee and others, released under GPL.
+
+# Support for StationPlaylsit Straemer
+# A standalone app used for broadcast streaming when using something other than Studio for broadcasting.
+
+import appModuleHandler
+import controlTypes
+from NVDAObjects.IAccessible import IAccessible
+
+class TEditNoLabel(IAccessible):
+
+	def _get_name(self):
+		return 			"Buffer Size {0} ms".format(self.value)
+
+
+class AppModule(appModuleHandler.AppModule):
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		# Try adding labels written to the screen in case edit fields are encountered.
+		if obj.windowClassName == "TEdit" and not obj.name and controlTypes.STATE_READONLY in obj.states:
+			clsList.insert(0, TEditNoLabel)
