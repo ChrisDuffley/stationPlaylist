2125551bfef119d730b2a6469e2e47a7a50ae1dd
Joseph Lee <joseph.lee22590@gmail.com>
2019-04-13 00:21:26 -0700

    SPL Engine: initial support for SPL DSP Engine (splengine).
    SPL DSP Engine is used for various things, including encoder support, more audio processing, and other tasks. This is actually a host process that hosts multiple DSP DLL's (including ones included in SPL Utils encoder support module).
    This initial commit adds support for detecting labels for SPL Encoder settings, the same work as SPL Streamer.
    This will be backported to LTS18 at some point in 2019.

---
+++ addon/appModules/splengine.py
@@ -0,0 +1,44 @@
+# StationPlaylist DSP Engine
+# Copyright 2019 Joseph Lee and others, released under GPL.
+
+# SPL Audio Processing Engine
+
+import appModuleHandler
+
+# For SPL encoder config screen at least, control iD's are different, which allows labels to be generated easily.
+encoderSettingsLabels= {
+	1008: "Quality",
+	1009: "Sample Rate",
+	1010: "Channels",
+	1013: "Server IP",
+	1014: "Server Port",
+	1015: "Encoder Password",
+	1016: "Mountpoint",
+	1017: "Server Type",
+	1018: "Encoder Type",
+	1019: ("Reconnect Seconds", "Server Name"),
+	1020: ("Encoder Username", "Server Description"),
+}
+
+class AppModule(appModuleHandler.AppModule):
+
+	def event_NVDAObject_init(self, obj):
+		if not obj.name:
+			# Same ID's are used for two different things, distinguishable by looking at which configuration ab is active.
+			windowControlID = obj.windowControlID
+			if windowControlID in (1019, 1020):
+				# Labels are split between parent window and the actual control, thus attribute error is seen.
+				try:
+					configTab = obj.parent.parent.previous.previous.previous.firstChild
+				except AttributeError:
+					configTab = obj.parent.parent.parent.previous.previous.previous.firstChild
+				obj.name = encoderSettingsLabels[windowControlID][0 if controlTypes.STATE_SELECTED in configTab.firstChild.states else 1]
+			else:
+				encoderSettingsLabel = encoderSettingsLabels.get(obj.windowControlID)
+				if encoderSettingsLabel:
+					obj.name = encoderSettingsLabel
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		# Try adding labels written to the screen in case edit fields are encountered.
+		if obj.windowClassName == "TEdit" and not obj.name and controlTypes.STATE_READONLY in obj.states:
+			clsList.insert(0, TEditNoLabel)
