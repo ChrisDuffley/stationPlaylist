31372833b5703519e464d95f901de3b7cd0e1ec0
Joseph Lee <joseph.lee22590@gmail.com>
2019-05-20 13:13:29 -0700

    Install tasks: 'StationPlaylist Studio/Studio' -> 'StationPlaylist'.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,4 +1,4 @@
-# StationPlaylist Studio add-on installation tasks
+# StationPlaylist add-on installation tasks
 # Copyright 2015-2019 Joseph Lee and others, released under GPL.
 
 # Provides needed routines during add-on installation and removal.
@@ -13,9 +13,9 @@ addonHandler.initTranslation()
 def onInstall():
 	# #17.12: Windows 7 SP1 or higher is required.
 	if sys.getwindowsversion().build < 7601:
-		# Translators: Presented when attempting to install Studio add-on on unsupported Windows releases.
+		# Translators: Presented when attempting to install StationPlaylist add-on on unsupported Windows releases.
 		gui.messageBox(_("You are using an older version of Windows. This add-on requires Windows 7 Service Pack 1 or later."),
-			# Translators: Title of a dialog shown when installing Studio add-on on old Windows releases.
+			# Translators: Title of a dialog shown when installing StationPlaylist add-on on old Windows releases.
 			_("Old Windows version"), wx.OK | wx.ICON_ERROR)
 		raise RuntimeError("SPL: minimum Windows version requirement not met, aborting")
 	import os, shutil
