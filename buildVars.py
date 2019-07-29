6ad86aa94c8530712bb9bc4c349d8e77ccf77938
Joseph Lee <joseph.lee22590@gmail.com>
2019-07-29 10:00:37 -0700

    StationPlaylist add-on 19.08
    Signed-off-by: Joseph Lee <joseph.lee22590@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -20,7 +20,7 @@ addon_info = {
 	"addon_description" : _("""Enhances support for StationPlaylist Studio.
 In addition, adds global commands for the studio from everywhere."""),
 	# version
-	"addon_version" : "19.07",
+	"addon_version" : "19.08",
 	# Author(s)
 	"addon_author" : u"Geoff Shang, Joseph Lee and other contributors",
 	# URL for the add-on documentation support
