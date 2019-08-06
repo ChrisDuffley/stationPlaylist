2b4e0eda9522b5100d336daeb6821c12f0cdf219
Joseph Lee <joseph.lee22590@gmail.com>
2019-08-06 09:06:59 -0700

    SPL add-on 18.09.11-LTS
    Signed-off-by: Joseph Lee <joseph.lee22590@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -20,7 +20,7 @@ addon_info = {
 	"addon_description" : _("""Enhances support for StationPlaylist Studio.
 In addition, adds global commands for the studio from everywhere."""),
 	# version
-	"addon_version" : "18.09.10-lts",
+	"addon_version" : "18.09.11-lts",
 	# Author(s)
 	"addon_author" : u"Geoff Shang, Joseph Lee and other contributors",
 	# URL for the add-on documentation support
