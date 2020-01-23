842e598ed31a7c0c08e075902f6692fe711c50b0
Joseph Lee <joseph.lee22590@gmail.com>
2020-01-23 07:14:54 -0800

    SPL Assistant: use the newly revised status objects fetcher routine for current track, next track, and temperature. Fixes #119.
    Rather than calling obj.firstChild, newly revised status fetcher would have correct objects (finally), so modify current track (C), next track (N), and temperature 9W) commands to use the new routine.

--- addon/appModules/splstudio/__init__.py
+++ addon/appModules/splstudio/__init__.py
@@ -2075,12 +2075,12 @@ class AppModule(appModuleHandler.AppModule):
 				# Translators: Presented when there is no information for the next track.
 				nextTrack = _("No track is playing")
 			else:
-				obj = self.status(self.SPLNextTrackTitle).firstChild
+				obj = self.status(self.SPLNextTrackTitle)
 				# Translators: Presented when there is no information for the next track.
 				nextTrack = _("No next track scheduled") if obj.name is None else obj.name
 			# #34 (17.08): normally, player position (name of the internal player in Studio) would not be announced, but might be useful for some broadcasters with mixers.
 			if splconfig.SPLConfig["SayStatus"]["SayStudioPlayerPosition"]:
-				player = self.status(self.SPLNextPlayer).firstChild.name
+				player = self.status(self.SPLNextPlayer).name
 				ui.message(", ".join([player, nextTrack]))
 			else:
 				ui.message(nextTrack)
@@ -2102,12 +2102,12 @@ class AppModule(appModuleHandler.AppModule):
 				# Translators: Presented when there is no information for the current track.
 				currentTrack = _("No track is playing")
 			else:
-				obj = self.status(self.SPLCurrentTrackTitle).firstChild
+				obj = self.status(self.SPLCurrentTrackTitle)
 				# Translators: Presented when there is no information for the current track.
 				currentTrack = _("Cannot locate current track information") if obj.name is None else obj.name
 			# #34 (17.08): see the note on next track script above.
 			if splconfig.SPLConfig["SayStatus"]["SayStudioPlayerPosition"]:
-				player = self.status(self.SPLCurrentPlayer).firstChild.name
+				player = self.status(self.SPLCurrentPlayer).name
 				ui.message(", ".join([player, currentTrack]))
 			else:
 				ui.message(currentTrack)
@@ -2125,7 +2125,7 @@ class AppModule(appModuleHandler.AppModule):
 			self.finish()
 			return
 		try:
-			obj = self.status(self.SPLTemperature).firstChild
+			obj = self.status(self.SPLTemperature)
 			# Translators: Presented when there is no weather or temperature information.
 			ui.message(obj.name if obj.name else _("Weather and temperature not configured"))
 		except RuntimeError:
