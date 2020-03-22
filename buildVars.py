fc87a221ada09d4b89834a61170a3901d22fa2be
Joseph Lee <joseph.lee22590@gmail.com>
2020-03-21 20:25:18 -0700

    BuildVars: remove minimum Windows version manifest flag.
    Minimum Windows version flag is specified but not built with the actual manifest, thus remove it.

--- buildVars.py
+++ buildVars.py
@@ -31,8 +31,6 @@ In addition, adds global commands for the studio from everywhere."""),
 	"addon_minimumNVDAVersion" : "2019.3.0",
 	# Last NVDA version supported/tested
 	"addon_lastTestedNVDAVersion" : "2020.1.0",
-	# Minimum Windows version supported
-	"addon_minimumWindowsVersion" : "6.1.7601",
 	# Add-on update channel (default is stable)
 	"addon_updateChannel" : None,
 }
