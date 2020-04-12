17085d306ab931407d60fe09f2425194bdff889b
Joseph Lee <joseph.lee22590@gmail.com>
2020-04-11 17:35:45 -0700

    Remote VT client: initial support for VT client. Re #124.
    Remote VT client is used by broadcasters to manage playlists stored on a remote station. As part of this, a remote playlist is presented to broadcasters, and one can query remaining time for the playlist. Based on playlist duration, one can record voice tracks to it won't exceed certain length.
    It turns out Remote VT playlist editor's user interface is similar tto playlist editor in Creator. In fact, the app module for Remote VT looks similar to Creator's except for no creator track item class and simplified constructor and terminate methods. Thus not only this app module will add playlist duration, it will also bring in things such as playlsit rotation/schedule and ability to review columns for tracks.

---
+++ addon/appModules/splremotevt.py
@@ -0,0 +1,116 @@
+# StationPlaylist Remote VT Client
+# An app module and global plugin package for NVDA
+# Copyright 2020 Joseph Lee and others, released under GPL.
+
+# Basic support for StationPlaylist Remote VT Client.
+# Borrows heavily from creator as the user interface is quite similar with changes specific to VT Client.
+
+import appModuleHandler
+import addonHandler
+import scriptHandler
+import globalVars
+import ui
+import api
+from .splstudio import SPLTrackItem
+addonHandler.initTranslation()
+
+class SPLRemotePlaylistEditorItem(SPLTrackItem):
+	"""An entry in SPL VT Remote Playlist Editor.
+	"""
+
+	# Keep a record of which column is being looked at.
+	_curColumnNumber = 0
+
+	__gestures={
+		"kb:control+alt+downArrow": None,
+		"kb:control+alt+upArrow": None,
+	}
+
+
+class AppModule(appModuleHandler.AppModule):
+
+	def __init__(self, *args, **kwargs):
+		super(AppModule, self).__init__(*args, **kwargs)
+		# Announce VT Client version at startup unless minimal flag is set.
+		try:
+			if not globalVars.appArgs.minimal:
+				# No translation.
+				ui.message("SPL VT Client {SPLVersion}".format(SPLVersion = self.productVersion))
+		except:
+			pass
+
+	def terminate(self):
+		super(AppModule, self).terminate()
+		# Clear Playlist Editor status cache, otherwise it will generate errors when Creator restarts without restarting NVDA.
+		self._playlistEditorStatusCache.clear()
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		import controlTypes
+		from NVDAObjects.IAccessible import sysListView32
+		# 20.02: tracks list uses a different window class name other than "TListView".
+		# Resort to window style and other tricks if other lists with the class name below is found and are not tracks list.
+		if obj.windowClassName == "TTntListView.UnicodeClass":
+			if obj.role == controlTypes.ROLE_LISTITEM:
+				clsList.insert(0, SPLRemotePlaylistEditorItem)
+			elif obj.role == controlTypes.ROLE_LIST:
+				clsList.insert(0, sysListView32.List)
+		elif obj.windowClassName in ("TDemoRegForm", "TAboutForm"):
+			from NVDAObjects.behaviors import Dialog
+			clsList.insert(0, Dialog)
+
+	# The following scripts are designed to work while using Playlist Editor.
+
+	def isPlaylistEditor(self):
+		if api.getForegroundObject().windowClassName != "TEditMain":
+			ui.message("You are not in playlist editor")
+			return False
+		return True
+
+	# Just like Studio, status items are scattered around the screen in Playlist Editor, and takes a really long time to fetch things.
+	SPLEditorDateTime = 0
+	SPLEditorDuration = 1
+	SPLEditorStatusBar = 2
+	_playlistEditorStatusCache = {}
+
+	@scriptHandler.script(gesture="kb:alt+NVDA+1")
+	def script_playlistDateTime(self, gesture):
+		if self.isPlaylistEditor():
+			try:
+				playlistHour = self._playlistEditorStatusCache[self.SPLEditorDateTime][0]
+				playlistDay = self._playlistEditorStatusCache[self.SPLEditorDateTime][1]
+			except KeyError:
+				playlistDateTime = api.getForegroundObject().simpleLastChild.firstChild.next
+				playlistHour = playlistDateTime.simpleNext
+				playlistDay = playlistHour.simpleNext.simpleNext
+				self._playlistEditorStatusCache[self.SPLEditorDateTime] = [playlistHour, playlistDay]
+			ui.message(" ".join([playlistDay.value, playlistHour.value]))
+
+	@scriptHandler.script(gesture="kb:alt+NVDA+2")
+	def script_playlistDuration(self, gesture):
+		if self.isPlaylistEditor():
+			try:
+				playlistDuration = self._playlistEditorStatusCache[self.SPLEditorDuration]
+			except KeyError:
+				playlistDuration = api.getForegroundObject().simpleLastChild.firstChild
+				self._playlistEditorStatusCache[self.SPLEditorDuration] = playlistDuration
+			ui.message(playlistDuration.name)
+
+	@scriptHandler.script(gesture="kb:alt+NVDA+3")
+	def script_playlistScheduled(self, gesture):
+		if self.isPlaylistEditor():
+			try:
+				statusBar = self._playlistEditorStatusCache[self.SPLEditorStatusBar]
+			except KeyError:
+				statusBar = api.getForegroundObject().firstChild.firstChild
+				self._playlistEditorStatusCache[self.SPLEditorStatusBar] = statusBar
+			ui.message(statusBar.getChild(2).name)
+
+	@scriptHandler.script(gesture="kb:alt+NVDA+4")
+	def script_playlistRotation(self, gesture):
+		if self.isPlaylistEditor():
+			try:
+				statusBar = self._playlistEditorStatusCache[self.SPLEditorStatusBar]
+			except KeyError:
+				statusBar = api.getForegroundObject().firstChild.firstChild
+				self._playlistEditorStatusCache[self.SPLEditorStatusBar] = statusBar
+			ui.message(statusBar.getChild(3).name)
