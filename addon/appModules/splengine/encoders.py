6c72ddf72baa484d5f24fe767afab18ae69a63c2
Joseph Lee <joseph.lee22590@gmail.com>
2020-05-01 19:08:53 -0700

    Encoder status announcement: do not include encoder ID when the only remaining live thread is the one the announcement function is running on. Fixes #135.
    Suppose a user enables background encoder monitoring for multiple encoders in one list, and somehow adds or deletes encoders. Under specific circumstances NVDA wil include encoder ID when announcing encoder status even though the only live thread is the one that is used to announce it. This was caused by ever-increasing encoder monitor counter, which will not decrease if an encoder is deleted by the user while being monitored unless monitoring is canceled (Control+F11 twice quickly). Therefore check how many monitor threads fo a given encoder type is alive instead of looking at monitor count to determine when to add encoder ID for status announcements.
    This also serves as a foundation for #136 (monitor threads reorganization).

--- addon/appModules/splengine/encoders.py
+++ addon/appModules/splengine/encoders.py
@@ -377,7 +377,10 @@ class Encoder(IAccessible):
 		# #79 (18.10.1/18.09.3-lts): wxPython 4 is more strict about where timers can be invoked from, and the below function is called from a thread other than the main one, which causes an exception to be logged.
 		# This is especially the case with some speech synthesizers and/or braille displays.
 		try:
-			if encoderMonCount[self.encoderType] > 1:
+			# #135 (20.06): find out how many background monitor threads for this encoder type are still active.
+			# #136 (20.07): encoder monitoring can cross encoder type boundaries (multiple encoder types can be monitored at once).
+			# Include encoder ID if multiple encoders have one encoder entry being monitored.
+			if len([thread for thread in self.threadPool.values() if thread.is_alive()]) > 1:
 				# Translators: Status message for encoder monitoring.
 				ui.message(_("{encoder} {encoderNumber}: {status}").format(encoder=self.encoderType, encoderNumber=encoderId, status=message))
 			else:
