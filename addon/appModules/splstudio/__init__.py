127075919531a4053be39d5d15a6027cd1fdc4ec
Joseph Lee <joseph.lee22590@gmail.com>
2020-06-28 23:47:59 -0700

    SPL track item: move directly to first/last column/child. Re #142.
    When invoking first/last column command from SPL track item class family, move directly to the first or last column without consulting current column number variable. Note that column count starts at 1.

--- addon/appModules/splstudio/__init__.py
+++ addon/appModules/splstudio/__init__.py
@@ -143,12 +143,12 @@ class SPLTrackItem(sysListView32.ListItem):
 	@scriptHandler.script(gesture="kb:control+alt+home")
 	def script_firstColumn(self, gesture):
 		self.__class__._curColumnNumber = 0
-		self._moveToColumnNumber(self._curColumnNumber+1)
+		self._moveToColumnNumber(1)
 
 	@scriptHandler.script(gesture="kb:control+alt+end")
 	def script_lastColumn(self, gesture):
 		self.__class__._curColumnNumber = self.parent.columnCount-1
-		self._moveToColumnNumber(self._curColumnNumber+1)
+		self._moveToColumnNumber(self.childCount)
 
 	@scriptHandler.script(
 		# Translators: input help mode message for column explorer commands.
