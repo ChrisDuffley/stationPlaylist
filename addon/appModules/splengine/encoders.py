8e3b6d5b329cbac108543adf07ac84f13336d219
Joseph Lee <joseph.lee22590@gmail.com>
2020-06-29 23:09:07 -0700

    Encoders: rename stream label to encoder label and convert it into a property/settiner combination. Re #139.
    When it comes to encoder labels, the term 'encoder label' is more appealing than just a 'stream label'. After all, the first item in encoder settings dialog is encoder label. Therefore rename 'streamLabel' to 'encoderLabel'. Also, convert get/set methods to property/setter combo for readability and to prevent messing with encoder label string value/setter.

--- addon/appModules/splengine/encoders.py
+++ addon/appModules/splengine/encoders.py
@@ -345,15 +345,16 @@ class Encoder(IAccessible):
 		return f"{self.encoderType} {self.IAccessibleChildID}"
 
 	# Get and set encoder labels (hence encoder label is not really a property, although it may appear to be so).
-	# 20.09: convert the following two methods to get/set descriptors.
 
-	def getStreamLabel(self):
+	@property
+	def encoderLabel(self):
 		try:
 			return SPLEncoderLabels[self.encoderId]
 		except KeyError:
 			return None
 
-	def setStreamLabel(self, newEncoderLabel):
+	@encoderLabel.setter
+	def encoderLabel(self, newEncoderLabel):
 		if len(newEncoderLabel):
 			SPLEncoderLabels[self.encoderId] = newEncoderLabel
 		else:
