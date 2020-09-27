87351416a04151aed25d8e139b6d5b152b427221
Joseph Lee <joseph.lee22590@gmail.com>
2020-09-26 19:21:35 -0700

    SPL config: transfer confspec to the new splconfspec module. Re #151.
    As splconfspec module will record add-on settings spec, remove the confspec defined in the config module as it is no longer used. Instead confspec definition in config module will point to splconfspec.confspec, and for backward compatibility, confspec will be imported directly.

--- addon/appModules/splstudio/splconfig.py
+++ addon/appModules/splstudio/splconfig.py
@@ -19,6 +19,7 @@ import gui
 import wx
 from . import splactions
 from . import spldebugging
+from .splconfspec import confspec
 import addonHandler
 addonHandler.initTranslation()
 from ..skipTranslation import translate
@@ -26,64 +27,6 @@ from ..skipTranslation import translate
 # Configuration management
 SPLIni = os.path.join(globalVars.appArgs.configPath, "splstudio.ini")
 SPLProfiles = os.path.join(globalVars.appArgs.configPath, "addons", "stationPlaylist", "profiles")
-# New (7.0) style config.
-confspec = ConfigObj(StringIO("""
-[General]
-BeepAnnounce = boolean(default=false)
-MessageVerbosity = option("beginner", "advanced", default="beginner")
-BrailleTimer = option("off", "intro", "outro", "both", default="off")
-AlarmAnnounce = option("beep", "message", "both", default="beep")
-TrackCommentAnnounce = option("off", "beep", "message", "both", default="off")
-LibraryScanAnnounce = option("off", "ending", "progress", "numbers", default="off")
-CategorySounds = boolean(default=false)
-TopBottomAnnounce = boolean(default=true)
-RequestsAlert = boolean(default=true)
-MetadataReminder = option("off", "startup", "instant", default="off")
-TimeHourAnnounce = boolean(default=true)
-ExploreColumns = string_list(default=list("Artist","Title","Duration","Intro","Outro","Category","Year","Album","Genre","Mood"))
-ExploreColumnsTT = string_list(default=list("Artist","Title","Duration","Cue","Overlap","Intro","Segue","Filename","Album","CD Code"))
-ExploreColumnsCreator = string_list(default=list("Artist","Title","Position","Cue","Intro","Outro","Segue","Duration","Last Scheduled","7 Days"))
-VerticalColumnAnnounce = option(None,"Status","Artist","Title","Duration","Intro","Outro","Category","Year","Album","Genre","Mood","Energy","Tempo","BPM","Gender","Rating","Filename","Time Scheduled",default=None)
-[PlaylistSnapshots]
-DurationMinMax = boolean(default=true)
-DurationAverage = boolean(default=true)
-ArtistCount = boolean(default=true)
-ArtistCountLimit = integer(min=0, max=10, default=5)
-CategoryCount = boolean(default=true)
-CategoryCountLimit = integer(min=0, max=10, default=5)
-GenreCount = boolean(default=true)
-GenreCountLimit = integer(min=0, max=10, default=5)
-ShowResultsWindowOnFirstPress = boolean(default=false)
-[IntroOutroAlarms]
-SayEndOfTrack = boolean(default=true)
-EndOfTrackTime = integer(min=1, max=59, default=5)
-SaySongRamp = boolean(default=true)
-SongRampTime = integer(min=1, max=9, default=5)
-[MicrophoneAlarm]
-MicAlarm = integer(min=0, max=7200, default="0")
-MicAlarmInterval = integer(min=0, max=60, default=0)
-[MetadataStreaming]
-MetadataEnabled = bool_list(default=list(false,false,false,false,false))
-[ColumnAnnouncement]
-UseScreenColumnOrder = boolean(default=true)
-ColumnOrder = string_list(default=list("Artist","Title","Duration","Intro","Outro","Category","Year","Album","Genre","Mood","Energy","Tempo","BPM","Gender","Rating","Filename","Time Scheduled"))
-IncludedColumns = string_list(default=list("Artist","Title","Duration","Intro","Outro","Category","Year","Album","Genre","Mood","Energy","Tempo","BPM","Gender","Rating","Filename","Time Scheduled"))
-IncludeColumnHeaders = boolean(default=true)
-[PlaylistTranscripts]
-ColumnOrder = string_list(default=list("Artist","Title","Duration","Intro","Outro","Category","Year","Album","Genre","Mood","Energy","Tempo","BPM","Gender","Rating","Filename","Time Scheduled"))
-IncludedColumns = string_list(default=list("Artist","Title","Duration","Intro","Outro","Category","Year","Album","Genre","Mood","Energy","Tempo","BPM","Gender","Rating","Filename","Time Scheduled"))
-[SayStatus]
-SayScheduledFor = boolean(default=true)
-SayListenerCount = boolean(default=true)
-SayPlayingCartName = boolean(default=true)
-SayStudioPlayerPosition = boolean(default=false)
-[Advanced]
-SPLConPassthrough = boolean(default=false)
-CompatibilityLayer = option("off", "jfw", default="off")
-[Startup]
-WelcomeDialog = boolean(default=true)
-"""), encoding="UTF-8", list_values=False)
-confspec.newlines = "\r\n"
 SPLConfig = None
 # The following settings can be changed in profiles:
 _mutatableSettings = ("IntroOutroAlarms", "MicrophoneAlarm", "MetadataStreaming", "ColumnAnnouncement")
