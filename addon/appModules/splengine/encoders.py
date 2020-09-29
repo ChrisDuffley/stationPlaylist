26f90ca334d3510ce0e477ed76a6fd2dd7b43a78
Joseph Lee <joseph.lee22590@gmail.com>
2020-09-29 00:21:11 -0700

    Encoder config dialog: add blank lines between settings items.
    Add a blank line between encoder config options for readability and to prepare for future Flake8 lint work.

--- addon/appModules/splengine/encoders.py
+++ addon/appModules/splengine/encoders.py
@@ -213,18 +213,22 @@ class EncoderConfigDialog(wx.Dialog):
 		# to set if NVDA should move focus to Studio window when connected.
 		self.focusToStudio = encoderConfigHelper.addItem(wx.CheckBox(self, label=_("&Focus to Studio when connected")))
 		self.focusToStudio.SetValue(obj.focusToStudio)
+
 		# Translators: A checkbox in encoder settings
 		# to set if NVDA should play the next track when connected.
 		self.playAfterConnecting = encoderConfigHelper.addItem(wx.CheckBox(self, label=_("&Play first track when connected")))
 		self.playAfterConnecting.SetValue(obj.playAfterConnecting)
+
 		# Translators: A checkbox in encoder settings
 		# to set if NVDA should monitor the status of this encoder in the background.
 		self.backgroundMonitor = encoderConfigHelper.addItem(wx.CheckBox(self, label=_("Enable background connection &monitoring")))
 		self.backgroundMonitor.SetValue(obj.backgroundMonitor)
+
 		# Translators: A checkbox in encoder settings
 		# to set if NVDA should play connection progress tone.
 		self.connectionTone = encoderConfigHelper.addItem(wx.CheckBox(self, label=_("Play connection status &beep while connecting")))
 		self.connectionTone.SetValue(obj.connectionTone)
+
 		# Translators: A checkbox in encoder settings
 		# to set if NVDA should announce connection progress until an encoder connects.
 		self.announceStatusUntilConnected = encoderConfigHelper.addItem(wx.CheckBox(self, label=_("Announce connection &status until encoder connects")))
