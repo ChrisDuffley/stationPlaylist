8138b8e489ad9280e8101e4ecfe999505ff9173b
Joseph Lee <joseph.lee22590@gmail.com>
2021-01-02 09:31:14 -0800

    Install tasks: remove Windows 7 check in order to align with NVDA Core schedule.
    Support for a given Windows release will now be tiedto a specific NVDA release to make maintenance easier.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -9,17 +9,6 @@ addonHandler.initTranslation()
 
 
 def onInstall():
-	import sys
-	import gui
-	import wx
-	# #17.12: Windows 7 SP1 or higher is required.
-	if sys.getwindowsversion().build < 7601:
-		gui.messageBox(
-			# Translators: Presented when attempting to install StationPlaylist add-on on unsupported Windows releases.
-			_("You are using an older version of Windows. This add-on requires Windows 7 Service Pack 1 or later."),
-			# Translators: Title of a dialog shown when installing StationPlaylist add-on on old Windows releases.
-			_("Old Windows version"), wx.OK | wx.ICON_ERROR)
-		raise RuntimeError("SPL: minimum Windows version requirement not met, aborting")
 	import os
 	import shutil
 	profiles = os.path.join(os.path.dirname(__file__), "..", "stationPlaylist", "profiles")
