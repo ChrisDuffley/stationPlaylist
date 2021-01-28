21f22b48e01e71bcf074e8b7d79eac190c680d6d
Joseph Lee <joseph.lee22590@gmail.com>
2021-01-27 20:07:37 -0800

    Studio/config save and reset action: do not proceed if Studio API returns None.
    Do not proceed with config save/reset action if Studio API returns None when trying to pass on status bar messages to do extra action method (used to enable/disable microphone alarm).
    This will be backported to lTS20.

--- addon/appModules/splstudio/__init__.py
+++ addon/appModules/splstudio/__init__.py
@@ -997,8 +997,9 @@ class AppModule(appModuleHandler.AppModule):
 	# Respond to profile switches if asked.
 	def actionProfileSwitched(self):
 		# #38 (17.11/15.10-LTS): obtain microphone alarm status.
-		# 21.03/20.09.6-LTS: only if Studio is still alive.
-		if splbase.studioIsRunning(justChecking=True):
+		# 21.03/20.09.6-LTS: only if Studio is still alive and Studio API says something.
+		status = splbase.studioAPI(2, 39)
+		if splbase.studioIsRunning(justChecking=True) and status is not None:
 			self.doExtraAction(self.sayStatus(2, statusText=True))
 
 	def actionSettingsReset(self, factoryDefaults=False):
@@ -1010,7 +1011,8 @@ class AppModule(appModuleHandler.AppModule):
 		if micAlarmT2 is not None:
 			micAlarmT2.Stop()
 		micAlarmT2 = None
-		if splbase.studioIsRunning(justChecking=True):
+		status = splbase.studioAPI(2, 39)
+		if splbase.studioIsRunning(justChecking=True) and status is not None:
 			self.doExtraAction(self.sayStatus(2, statusText=True))
 
 	# Alarm announcement: Alarm notification via beeps, speech or both.
