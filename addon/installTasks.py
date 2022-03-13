b47b5d8c27efb413a494465b5597e595ff512ed3
Joseph Lee <joseph.lee22590@gmail.com>
2022-03-12 19:47:18 -0800

    Install tasks: add old Windows version message.
    On Windows releases earlier than 10, display a message informing users that this is the last version of the add-on to support old Windows releases. This is in preparation for ending support for Windows 7 SP1, 8, and 8.1 in 2022.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -11,6 +11,20 @@ addonHandler.initTranslation()
 def onInstall():
 	import os
 	import shutil
+	import winVersion
+	import gui
+	import wx
+	if winVersion.getWinVer() < winVersion.WIN10:
+		gui.messageBox(
+			_(
+				# Translators: Dialog text shown when attempting to install the add-on on an older version of Windows.
+				"You are using an older version of Windows. "
+				"This is the last version of StationPlaylist add-on to support this version of Windows. "
+				"Future add-on releases will require Windows 10 or later."
+			),
+			# Translators: title of the dialog shown when trying to install the add-on on an old version of Windows.
+			_("Old Windows version"), wx.OK | wx.ICON_WARNING
+		)
 	profiles = os.path.join(os.path.dirname(__file__), "..", "stationPlaylist", "profiles")
 	# Import old profiles.
 	if os.path.exists(profiles):
