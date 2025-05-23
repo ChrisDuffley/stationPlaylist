2b30749ccd4d733c5783a4a9f650c9207b06629f
Joseph Lee <joseph.lee22590@gmail.com>
2025-05-23 07:54:56 -0600

    Studio app module: minimum Studio version -> 6.0

--- addon/appModules/splstudio/__init__.py
+++ addon/appModules/splstudio/__init__.py
@@ -10,7 +10,7 @@
 # For SPL Studio Controller, focus movement and other utilities,
 # see the global plugin version of this app module.
 
-# Minimum version: SPL 5.50, NVDA 2024.1.
+# Minimum version: SPL 6.0, NVDA 2024.1.
 
 from typing import Any, Callable
 from functools import wraps
@@ -72,7 +72,7 @@ def finally_(func, final):
 
 
 # Make sure the broadcaster is running a compatible version.
-SPLMinVersion = "5.50"
+SPLMinVersion = "6.0"
 
 # Threads pool.
 micAlarmT: threading.Timer | None = None
