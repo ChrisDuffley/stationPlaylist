563c8ff1a937d9c0704093ddb02a4d5be2e5098c
Joseph Lee <joseph.lee22590@gmail.com>
2025-05-24 09:56:38 -0600

    SPL common services: create a copy of SPL base services module inside splcommon package.
    The SPL common services (splcommon) is a package of base modules used across both the app modules set and the global plugin and support modules for them. The aim is to house common code across the app modules set and the global plugin in hopes of reducing code duplication. This also means the common services package is responsible for overall functioning of the add-on. To acknowledge that Studio ap module is at the top of the app module set/global plugin hierarchy for thsi add-on, splcommon package is placed inside appModules folder with initial code coming from the Studio app module components used across the add-on.
    Note: this is a continuation of recent code refactoring and restructuring efforts including Python 3.11 upgrade and Studio 6.x requirement.

---
+++ addon/appModules/splcommon/splbase.py
@@ -0,0 +1,65 @@
+# SPL Studio base services
+# An app module and global plugin package for NVDA
+# Copyright 2018-2025 Joseph Lee, released under GPL.
+
+# Base services for Studio app module and support modules
+
+import ui
+from winUser import sendMessage, user32
+from logHandler import log
+import addonHandler
+
+addonHandler.initTranslation()
+
+# Cache the handle to main Studio window.
+_SPLWin: int | None = None
+
+# Various SPL IPC tags.
+SPLSelectTrack = 121
+
+# Check if Studio itself is running.
+# This is to make sure custom commands for SPL Assistant commands
+# and other app module gestures display appropriate error messages.
+# Some checks will need to omit message output.
+def studioIsRunning(justChecking: bool = False) -> bool:
+	# Keep the boolean flag handy because of message output.
+	isStudioAlive = (_SPLWin is not None and _SPLWin == user32.FindWindowW("SPLStudio", None)) or (
+		_SPLWin is None and user32.FindWindowW("SPLStudio", None) != 0
+	)
+	if not isStudioAlive:
+		log.debug("SPL: Studio is not alive")
+		if not justChecking:
+			# Translators: A message informing users that Studio is not running so certain commands will not work.
+			ui.message(_("Studio main window not found"))
+	return isStudioAlive
+
+
+# Use SPL Studio API to obtain needed values.
+# A thin wrapper around user32.SendMessage function with Studio handle and WM_USER supplied.
+# #45: returns whatever result SendMessage function says.
+# If NVDA is in debug mode, print arg, command and other values.
+# Strengthen this by checking for the handle once more.
+# #92: SendMessage function returns something from anything (including from dead window handles),
+# so really make sure Studio window handle is alive.
+def studioAPI(arg: int, command: int) -> int | None:
+	if not studioIsRunning(justChecking=True):
+		return None
+	# Global plugin can also call this function with no Studio window handle value defined.
+	global _SPLWin
+	if _SPLWin is None:
+		_SPLWin = user32.FindWindowW("SPLStudio", None)
+	log.debug(f"SPL: Studio API wParem is {arg}, lParem is {command}")
+	val = sendMessage(_SPLWin, 1024, arg, command)
+	log.debug(f"SPL: Studio API result is {val}")
+	# SendMessage function might be stuck while Studio exits, resulting in NULL window handle.
+	if not user32.FindWindowW("SPLStudio", None):
+		val = None
+		log.debug("Studio window is gone, Studio API result is None")
+	return val
+
+
+# Select a track upon request.
+def selectTrack(trackIndex: int) -> None:
+	studioAPI(-1, SPLSelectTrack)
+	log.debug(f"SPL: selecting track index {trackIndex}")
+	studioAPI(trackIndex, SPLSelectTrack)
