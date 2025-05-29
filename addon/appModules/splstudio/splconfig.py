bd7639edab961954ad0bed59ed6abaf6b4cc38b1
Joseph Lee <joseph.lee22590@gmail.com>
2025-05-28 19:58:58 -0600

    SPL config/show startup dialogs: remove unused 'oldVer' flag.
    the purpose of the now removed old version flag was to inform users about long-term support channel availability. This was possible because Ad-on Updater (now unsupported) allowed add-ons to define additional update channels, and SPL add-on took advantage of this - offering LTS channel to support oldest supported Studio release. However, NV Access add-on store only supports stable, beta, and dev channels, meaning LTS users must download LTS releases manually. While it could allow old version flag to be used to inform users about upcoming LTS releases (changing the messaging to say that manual download is required), this facility hasn't been used in years. Thus, remove the now unused flag to adopt to changing times and reduce confusion in the long-term.

--- addon/appModules/splstudio/splconfig.py
+++ addon/appModules/splstudio/splconfig.py
@@ -863,12 +863,7 @@ Thank you.""")
 
 
 # And to open the above dialog and any other dialogs.
-# 18.09: return immediately after opening old ver dialog if minimal flag is set.
-def showStartupDialogs(oldVer: bool = False) -> None:
-	# Old version reminder if this is such a case.
-	# 17.10: and also used to give people a chance to switch to LTS.
-	# 20.06: controlled by a temporary flag that can come and go.
-	# To be resurrected later.
+def showStartupDialogs() -> None:
 	if globalVars.appArgs.minimal:
 		return
 	if SPLConfig["Startup"]["WelcomeDialog"]:
