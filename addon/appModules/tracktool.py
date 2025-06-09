a1c4c91f25e006f6232392d2119c9796e271b826
Joseph Lee <joseph.lee22590@gmail.com>
2025-06-09 08:37:19 -0600

    Track Tool: announce app versoin at startup.
    Common feature: announce Track Tool version at startup. This can lead to double announcement as Track Tool shows version info on the title bar.

--- addon/appModules/tracktool.py
+++ addon/appModules/tracktool.py
@@ -10,6 +10,8 @@
 import appModuleHandler
 import addonHandler
 import tones
+import globalVars
+import ui
 import controlTypes
 from NVDAObjects.IAccessible import sysListView32
 from .splstudio import splconfig, SPLTrackItem
@@ -155,6 +157,13 @@ class TrackToolItem(SPLTrackItem):
 class AppModule(appModuleHandler.AppModule):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+		# Announce app version at startup unless minimal flag is set.
+		try:
+			if not globalVars.appArgs.minimal:
+				# No translation.
+				ui.message("{} {}".format(self.productName, self.productVersion))
+		except Exception:
+			pass
 		# #64: load config database if not done already.
 		splconfig.openConfig("tracktool")
 
