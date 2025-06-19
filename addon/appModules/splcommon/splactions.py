d199e5779f0fa5d99989260e02ec550d7591b5e5
Joseph Lee <joseph.lee22590@gmail.com>
2025-06-19 08:08:55 -0600

    SPL common services: transfer SPL actions from Studio app module to commone services.
    SPL actions include extension points to control Studio app module such as closing dialogs at app termination, profile switches, and other common services. However, as SPL Engine is also using parts of this module, the engine app module needed to import splstudio.splactions at startup, specificlaly to close encoder config dilaog when the engine terminates. Thus, create a SPL common services version of SPL actions module so the Engine app module does not have to rely on Studio but import actions from common services directly. This also allows breaking some of the import cycles reported by Pyright.

---
+++ addon/appModules/splcommon/splactions.py
@@ -0,0 +1,19 @@
+# SPL Studio Actions
+# An app module and global plugin package for NVDA
+# Copyright 2017-2025 Joseph Lee, released under GPL.
+# Defines various actions to be carried out by various add-on modules, powered by extension points.
+# This module provides services for other modules, not the other way around.
+# Thus, importing other add-on modules should not be attempted unless granted.
+# Transferred from Studio app module package to common services in 2025.
+
+# An action is a notification about something that has happened or about to happen.
+# In SPL add-on, an action is prefixed by SPLAction*.
+
+import extensionPoints
+
+# Switching broadcast profiles.
+SPLActionProfileSwitched = extensionPoints.Action()
+# Settings are reloading or set to factory defaults.
+SPLActionSettingsReset = extensionPoints.Action()
+# Studio is terminating.
+SPLActionAppTerminating = extensionPoints.Action()
