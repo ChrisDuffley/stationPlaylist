515cfc37ef52f60e75cffa97a82ef37be0344bcd
Joseph Lee <joseph.lee22590@gmail.com>
2025-08-22 21:16:59 -0600

    StationPlaylist 25.09
    Signed-off-by: Joseph Lee <joseph.lee22590@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -23,7 +23,7 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	addon_description=_("Enhances support for StationPlaylist apps. In addition, adds global commands for the Studio application from everywhere."),
 	# version
-	addon_version="25.08",
+	addon_version="25.09",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
 	addon_changelog=_("""* NVDA 2025.1 or later is required.
