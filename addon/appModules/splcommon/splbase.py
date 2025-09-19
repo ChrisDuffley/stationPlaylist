c27a0847a1202b72ffcfe132433accdb807207a6
Joseph Lee <joseph.lee22590@gmail.com>
2025-09-19 13:29:07 -0600

    SPL common/base (NVdA 2026.1 compatibility): import user32.dll from winBindings if possible (winUser.user32 import is deprecated).

--- addon/appModules/splcommon/splbase.py
+++ addon/appModules/splcommon/splbase.py
@@ -12,10 +12,15 @@ import api
 import windowUtils
 import scriptHandler
 import globalVars
-from winUser import sendMessage, user32
+from winUser import sendMessage
 from logHandler import log
 from NVDAObjects.IAccessible import sysListView32
 import addonHandler
+# From NVDA 2026.1 onwards, winBindings package should be used to look for Windows API dll's.
+try:
+	from winBindings.user32 import dll as user32
+except ModuleNotFoundError:
+	from winUser import user32
 
 addonHandler.initTranslation()
 
