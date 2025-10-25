1bfeda989a8d8f0d0537c1fa054fc49288273dd3
Joseph Lee <joseph.lee22590@gmail.com>
2025-10-24 20:26:58 -0600

    Studio (local/base): add a private flag on requiring Studio API.
    Loca Studio (splstudio) reuqires Studio API for full functionality whereas Remote Studio does not. Without this flag, performing Studio add-on commands from Remote Studio will cause NVDA to say 'Studio main window not found'. Therefore, create a private flag to inform Studio app modules family if Studio API is really needed for an SPL component (true on local Studio, false on Remote Studio).

--- addon/appModules/splstudio/__init__.py
+++ addon/appModules/splstudio/__init__.py
@@ -700,6 +700,8 @@ class AppModule(appModuleHandler.AppModule):
 	_focusedTrack = None
 	# Monitor Studio API routines.
 	_SPLStudioMonitor = None
+	# Does this SPL component rely on Studio API?
+	_studioAPIRequired = True  # Studio requires its API for full functionality
 
 	# Prepare the settings dialog among other things.
 	def __init__(self, *args, **kwargs):
