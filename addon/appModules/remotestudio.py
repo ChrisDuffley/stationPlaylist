95114c78216d35af80aee248bd224cc39a05c4b7
Joseph Lee <joseph.lee22590@gmail.com>
2025-10-24 19:42:51 -0600

    SPL Remote Studio: initial version.
    SPL Remote Studio allows broadcasters to remotely access and control a Studio (local) installation from a remote location. The user interface is almost identical to Studio (local/on premise) except the program is optimized for remote broadcasting such as limiting cart slots and toggling remote sound output. Therefore, rather than an app alias, a dedicated Remote Studio app module (remotestudio) will be introduced to add support for it.

---
+++ addon/appModules/remotestudio.py
@@ -0,0 +1,11 @@
+# StationPlaylist Remote Studio
+# An app module and global plugin package for NVDA
+# Copyright 2025 Joseph Lee, released under GPL.
+
+# Basic support for StationPlaylist Remote Studio.
+# Borrows heavily from Studio as the user interface is quite similar with changes specific to Remote Studio.
+
+from . import splstudio
+
+class AppModule(splstudio.AppModule):
+	pass
