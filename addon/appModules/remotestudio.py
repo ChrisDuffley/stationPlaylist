e9d8237584e1869dc72aa8b17855eff694e30d49
Joseph Lee <joseph.lee22590@gmail.com>
2025-10-26 00:43:53 -0600

    Remote Studio: limit Cart Explorer to twelve (function key) carts.
    Remote Studio defines F1 through F12 as cart keys (twelve slots), either playing a local file (local cart) or a cart slot defined in Studio running somewhere (Studio cart). Therefore, in the Remote Studio app module, carts builder will work with function keys (a dedicated carts builder method is defined in Remote Studio to replace the local Studio version).

--- addon/appModules/remotestudio.py
+++ addon/appModules/remotestudio.py
@@ -5,8 +5,27 @@
 # Basic support for StationPlaylist Remote Studio.
 # Borrows heavily from Studio as the user interface is quite similar with changes specific to Remote Studio.
 
+from typing import Any
 from . import splstudio
+from .splstudio import splmisc
 
 class AppModule(splstudio.AppModule):
 	# Remote Studio does not require Studio API to function.
 	_studioAPIRequired = False
+
+	# Cart explorer (Remote Studio)
+	cartExplorer = False
+	# The carts dictionary (key = cart gesture, item = cart name).
+	carts: dict[str, Any] = {}
+
+	# Assigning and building carts.
+
+	def cartsBuilder(self, build: bool = True) -> None:
+		# A function to build and return cart commands.
+		# Remote Studio offers twelve carts (function keys only).
+		if build:
+			for cart in splmisc.cartKeys[:12]:
+				self.bindGesture(f"kb:{cart}", "cartExplorer")
+		else:
+			self.clearGestureBindings()
+			self.bindGestures(self.__gestures)
