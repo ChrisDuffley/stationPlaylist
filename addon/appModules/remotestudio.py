7ed3cb98d376b43d4eb8f82206ea3760310f8ba9
Joseph Lee <joseph.lee22590@gmail.com>
2025-11-02 16:39:40 -0700

    Remote Studio: define screen traversal routes.
    Remote Studio may not offer an API to obtain playlist times, elapsed and remaining time, and other information. therefore, traverse the screen like really old SPL add-on releases.

--- addon/appModules/remotestudio.py
+++ addon/appModules/remotestudio.py
@@ -34,3 +34,42 @@ class AppModule(splstudio.AppModule):
 	# Check to make sure a playlist is indeed loaded by checking track count.
 	def playlistLoaded(self) -> bool:
 		return bool(getattr(api.getFocusObject(), "rowCount", 0))
+
+	# Status table keys
+	# Unlike local Studio, Remote Studio may not have an API, so screen traversal must be done.
+	# This is applicable to SPL Assistant commands and other scripts.
+	SPLRemoteStatus = 0
+	SPLTrackRemainingTime = 1
+	SPLTrackElapsedTime = 2
+	SPLNextTrackTitle = 3
+	SPLCurrentTrackTitle = 4
+	SPLTrackStarts = 5
+	SPLTrackStartsIn = 6
+	SPLPlaylistRemain = 7
+	SPLTotalForHour = 8
+	SPLTemperature = 9
+
+	# Table of child constants based on versions
+	# These are scattered throughout the screen, so one can use foreground.getChild(index) to fetch them
+	# (getChild tip from Jamie Teh (NV Access/Mozilla)).
+	# Because 6.x an possible future releases may use different screen layouts,
+	# look up the needed constant from the table below
+	# (row = info needed, column = version).
+	# As of 2025, the below table is based on Studio 6.0.
+	# #119: a list indicates iterative descent to locate the actual objects.
+	statusObjs = {
+		"6": {
+			SPLRemoteStatus: [2, 0],  # Remote Studio status bar
+			SPLTrackRemainingTime: [2, 2, -1],
+			SPLTrackElapsedTime: [2, 2, -2],
+			SPLNextTrackTitle: [2, 2, 2],
+			SPLCurrentTrackTitle: [2, 2, 9],
+			SPLTrackStarts: [2, 2, -3],
+			SPLTrackStartsIn: [2, 2, -6],
+			SPLPlaylistRemain: [2, 2, -5],
+			SPLTotalForHour: [2, 2, -4],
+			SPLTemperature: [-1, 1, 3, 0],  # Temperature for the current city.
+		},
+	}
+
+	_cachedStatusObjs: dict[int, Any] = {}
