8a24a44ae9455d8960d72cd71adcfa3d8237ab03
Joseph Lee <joseph.lee22590@gmail.com>
2025-11-09 01:52:36 -0700

    StationPlaylist add-on 25.11
    Signed-off-by: Joseph Lee <joseph.lee22590@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -23,7 +23,7 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	addon_description=_("Enhances support for StationPlaylist apps. In addition, adds global commands for the Studio application from everywhere."),
 	# version
-	addon_version="25.10",
+	addon_version="25.11",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
 	addon_changelog=_("""* Initial support for StationPlaylist suite 6.20.
