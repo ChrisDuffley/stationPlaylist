00c0b3b6a2186739482ed9a95f50305e012110b3
Joseph Lee <joseph.lee22590@gmail.com>
2025-11-17 12:14:11 -0700

    Local Studio: make sure playlist viewer is focused before checking window style values. Re #170.
    Turns out local Studio 6.20 uses same window style values for main window and insert tracks track list. Therefore, check if the simple grandparent for the list item says this is the main Studio window (window class == TStudioForm). This resolves oddities such as top and bottom beeps heard when browsing insert tracks results list and column position and index issue in columns explorer.

--- addon/appModules/splstudio/__init__.py
+++ addon/appModules/splstudio/__init__.py
@@ -961,10 +961,16 @@ class AppModule(appModuleHandler.AppModule):
 			case "TTntListView.UnicodeClass":
 				if role == controlTypes.Role.LISTITEM:
 					trackItemWindowStyle = 1443958849 if self.productVersion >= "6.20" else 1443991617
-					if trackItemWindowStyle in (
-						1443991617,  # Studio 6.11 and earlier
-						1443958849  # Studio 6.20
-					) and abs(windowStyle - trackItemWindowStyle) % 0x100000 == 0:
+					# In local Studio 6.20, insert tracks dialog's tracks list
+					# has same window style as playlist viewer.
+					# Ask simple grandparent object if this is STudio main window.
+					if (
+						obj.simpleParent.simpleParent.windowClassName == "TStudioForm"
+						and trackItemWindowStyle in (
+							1443991617,  # Studio 6.11 and earlier
+							1443958849  # Studio 6.20
+						) and abs(windowStyle - trackItemWindowStyle) % 0x100000 == 0
+					):
 						clsList.insert(0, StudioPlaylistViewerItem)
 					else:
 						clsList.insert(0, SPLStudioTrackItem)
