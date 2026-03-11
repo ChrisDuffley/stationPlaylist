cf639d9aaf798d5b61b3f67ba6456e1b941a018c
Joseph Lee <joseph.lee22590@gmail.com>
2026-03-11 04:27:09 -0600

    SPL common services/Studio track comments (security): no longer save a pickle version of track comments serialization data.
    Pickle in insecure, therefore do not save a copy of track comments data using this protocol (use json instead).

--- addon/appModules/splcommon/splconfig.py
+++ addon/appModules/splcommon/splconfig.py
@@ -712,9 +712,6 @@ def terminateStudioExtraSteps() -> None:
 	if not globalVars.appArgs.secure:
 		with open(os.path.join(globalVars.appArgs.configPath, "spltrackcomments.json"), "w") as f:
 			json.dump(trackComments, f)
-		# 26.03.1 (security/compatibility_: save a pickle copy to be discarded in future add-on releases.
-		with open(os.path.join(globalVars.appArgs.configPath, "spltrackcomments.pickle"), "wb") as f:
-			pickle.dump(trackComments, f, protocol=4)
 	trackComments.clear()
 	trackComments = {}
 
