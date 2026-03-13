28a96218907b833184ae0d5d09c5ba3eb93c042b
Joseph Lee <joseph.lee22590@gmail.com>
2026-03-13 17:34:49 -0600

    Creator: remove 6.0x window style when detecting track list items

--- addon/appModules/splcreator.py
+++ addon/appModules/splcreator.py
@@ -164,7 +164,6 @@ class AppModule(appModuleHandler.AppModule):
 				if obj.parent.simpleParent.windowClassName == "TEditMain":
 					clsList.insert(0, SPLPlaylistEditorItem)
 				elif obj.windowStyle in (
-					1443958857,  # Creator 6.0x
 					1443958849,  # Creator 6.1x
 					1446056001,  # Creator 6.20
 				) and obj.parent.simpleParent.windowClassName == "TTabSheet":  # Creator main window
