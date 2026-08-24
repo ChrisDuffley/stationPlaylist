d34047ee152f2c7a5d876b8710f3d6ad9961e99f
Joseph Lee <joseph.lee22590@gmail.com>
2026-08-24 17:10:01 -0600

    Revert "Local and Remote Studio, Track Tool, Creator: mark overlay classes chooser as overrides for base app module"
    This reverts commit 2f19972828aadf02c28af4ebf9109594faddb105 - the base app module's overlay classes chooser is gone.

--- addon/appModules/splstudio/__init__.py
+++ addon/appModules/splstudio/__init__.py
@@ -10,7 +10,7 @@
 # For SPL Studio Controller, focus movement and other utilities,
 # see the global plugin version of this app module.
 
-from typing import Any, NamedTuple, override, ClassVar
+from typing import Any, NamedTuple, ClassVar
 import os
 import time
 import threading
@@ -666,7 +666,6 @@ class AppModule(splappmod.AppModule):
 			self._analysisMarker = None
 
 	# Some controls which needs special routines.
-	@override
 	def chooseNVDAObjectOverlayClasses(self, obj: NVDAObject, clsList: list[NVDAObject]) -> None:
 		role = obj.role
 		# #173: check if breakNoteDialog can be used before checking unlabeled controls.
