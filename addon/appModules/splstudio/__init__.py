50ca1c67056c1110d0274c11fb97563e0272f269
Joseph Lee <joseph.lee22590@gmail.com>
2026-08-25 01:28:59 -0600

    Local and Remote Studio/track finder: pass in slfind module's search entries as search history list argument (alignig with NVDA Core)

--- addon/appModules/splstudio/__init__.py
+++ addon/appModules/splstudio/__init__.py
@@ -1284,6 +1284,7 @@ class AppModule(splappmod.AppModule):
 				title,
 				directionForward=directionForward,
 				columnSearch=columnSearch,
+				searchEntries=splfind.searchEntries,
 			)
 			gui.mainFrame.prePopup()
 			d.Raise()
