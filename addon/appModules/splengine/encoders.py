31d5e79c21a2055e2898479e2528736e52647af9
Joseph Lee <joseph.lee22590@gmail.com>
2026-09-03 23:45:11 -0600

    Encoders/column announcement: use implicit index when announcing column header and data for encoder items

--- addon/appModules/splengine/encoders.py
+++ addon/appModules/splengine/encoders.py
@@ -624,7 +624,7 @@ class Encoder(IAccessible):
 
 	# Announce encoder specific data (located in columns unless noted otherwise).
 	def announceEncoderData(self, column: int) -> None:
-		ui.message("{0}: {1}".format(self.getChild(column).columnHeaderText, self.getChild(column).name))
+		ui.message("{}: {}".format(self.getChild(column).columnHeaderText, self.getChild(column).name))
 
 	def initOverlayClass(self):
 		# Load encoder settings upon request.
