6ef86f7da52a164445b99e35ce8df0eaed7c0117
Joseph Lee <joseph.lee22590@gmail.com>
2026-09-10 12:36:14 -0600

    StationPlaylist add-on 26.07.5-LTS
    Signed-off-by: Joseph Lee <joseph.lee22590@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -23,7 +23,7 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	addon_description=_("Enhances support for StationPlaylist apps. In addition, adds global commands for the Studio application from everywhere."),
 	# version
-	addon_version="26.07.4",
+	addon_version="26.07.5",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
 	addon_changelog=_("""* NVDA will no longer announce wrong information when reporting remaining time for a playing track via SPL Controller layer (R) when running both local and Remote Studio. Specifically, NVDA will no longer include cart remaining time when SPL Controler coverage is set to Remote Studio."""),
